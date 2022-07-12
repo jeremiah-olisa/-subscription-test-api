abf1495aea69b57edd21174b3ed1cf1d7aa3205c
Jeremiah Olisa <jeremiaholisa453@gmail.com>
2022-07-12 01:02:51 +0100

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory()
-            ->count(25)
-            ->websites(10)
-            ->posts(30)
-            ->subscriptions(3)
-            ->create();
-        
+        // \App\Models\User::factory(10)->create();
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
